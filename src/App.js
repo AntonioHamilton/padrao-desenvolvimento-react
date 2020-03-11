16858448513ff852425b43f187b98ac990cf87be
Antonio Hamilton <antoniohamiltonfreitas@outlook.com>
2020-03-10 23:14:54 -0300

    Padrão projeto em React

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import { GlobalStyle } from "./Styles/Global";
+
+import Routes from "./routes";
+
+const App = () => (
+  <>
+    <GlobalStyle />
+    <Routes />
+  </>
+);
+
+export default App;
